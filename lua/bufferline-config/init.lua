da1668bb4507d4123fd0e6727ac2d0112ac8b00b
Cleo Menezes Jr <produtor.junior.menezes@gmail.com>
2022-02-21 10:50:24 -0300

    fix: bufferline - close command bufdelete

--- lua/bufferline-config/init.lua
+++ lua/bufferline-config/init.lua
@@ -1,7 +1,7 @@
 require("bufferline").setup {
   options = {
-    close_command = "bdelete! %d",       -- can be a string | function, see "Mouse actions"
-    right_mouse_command = "bdelete! %d", -- can be a string | function, see "Mouse actions"
+    close_command = "Bdelete %d",       -- can be a string | function, see "Mouse actions"
+    right_mouse_command = "Bdelete %d", -- can be a string | function, see "Mouse actions"
     left_mouse_command = "buffer %d",    -- can be a string | function, see "Mouse actions"
     buffer_close_icon = '',
     modified_icon = '',
